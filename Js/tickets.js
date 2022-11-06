17e39312e38157920594d161f9165d78de819566
Lautaro <lautarocelestino92@gmail.com>
2022-11-06 12:01:47 -0300

    primer repositorio

---
+++ Js/tickets.js
@@ -0,0 +1,56 @@
+function calcularTotal() {
+    var cantEntradas,
+      total,
+      categoria,
+      entradaEstudiante,
+      entradaTrainee,
+      entradaJr,
+      general;
+  
+    console.log('entra a la funcion');
+    cantEntradas = document.getElementById('inputCantidad').value;
+    categoria = document.getElementById('inputCategoria').value;
+  
+    console.log('cant entradas ' + cantEntradas + ' categoria ' + categoria);
+  
+    entradaEstudiante = 200 - (200 * 80) / 100;
+    entradaTrainee = 200 - (200 * 50) / 100;
+    entradaJr = 200 - (200 * 15) / 100;
+    general = 200;
+  
+    /*Mensaje de alerta si no se ingresa cantidad de entradas*/
+
+    if (cantEntradas > 0 && !isNaN(cantEntradas) && cantEntradas % 1 == 0) {
+
+      //Se calcula el monto de cada variable
+
+      if (categoria == 'Estudiante') {
+        total = entradaEstudiante * cantEntradas;
+        document.getElementById('importeTotal').innerHTML =
+          'Total a Pagar: $ ' + total;
+      } else if (categoria == 'Trainee') {
+        total = entradaTrainee * cantEntradas;
+        document.getElementById('importeTotal').innerHTML =
+          'Total a Pagar: $ ' + total;
+      } else if (categoria == 'Junior') {
+        total = entradaJr * cantEntradas;
+        document.getElementById('importeTotal').innerHTML =
+          'Total a Pagar: $ ' + total;
+      } else if (categoria == 'General') {
+        total = general * cantEntradas;
+        document.getElementById('importeTotal').innerHTML =
+          'Total a Pagar: $ ' + total;
+      }
+    } else {
+      window.alert('Debe ingresar la cantidad de entradas en numeros');
+    }
+  }
+  
+  function borrar() {
+    document.getElementById('inputNombre').value = '';
+    document.getElementById('inputApellido').value = '';
+    document.getElementById('inputEmail').value = '';
+    document.getElementById('inputCantidad').value = '';
+    document.getElementById('inputCategoria').value = 'Estudiante';
+    document.getElementById('importeTotal').innerHTML = 'Total a Pagar: $ ';
+  }
